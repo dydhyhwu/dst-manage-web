2510fa4a747548e33669649457822c3a5584fd98
dydhyhwu <dydhyh@foxmail.com>
2023-08-14 22:48:32 +0800

    feat: 初始化项目

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    // 'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react',
+    'react-hooks',
+    'react-refresh',
+    'prettier',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
---
+++ .old/README.md
@@ -0,0 +1,16 @@
+# 饥荒管理控制台页面
+
+## 技术栈
++ react
++ antdesgin ui
+
+## 启动
+```
+npm install
+npm run start
+```
+
+## 打包
+```
+npm run build
+```
